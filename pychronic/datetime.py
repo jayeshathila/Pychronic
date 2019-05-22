a9c187de8325b15325e2dfe4012bd0c550ba0e68
jayeshathila <sharma.jayesh52@gmail.com>
2019-05-22 14:08:54 +0530

    add day_of_week to pychronic_datetime

--- pychronic/datetime.py
+++ pychronic/datetime.py
@@ -1,10 +1,11 @@
 # Exposing class to use the fields as per end users convinience
 class PychronicDatetime:
-    def __init__(self, day: str, time: str, month: str, year: int):
+    def __init__(self, day: str, time: str, month: str, year: int, day_of_week: str):
         self.day = day
         self.time = time
         self.month = month
         self.year = year
+        self.day_of_week = day_of_week
 
     def __repr__(self):
         return f"{self.day} {self.month} {self.year}, {self.time}"
