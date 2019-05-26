269195d40794ff45a0c8ecb1f87a4408ef3f3e31
jayeshathila <sharma.jayesh52@gmail.com>
2019-05-26 11:33:08 +0530

    add datatype enums

--- pychronic/enums.py
+++ pychronic/enums.py
@@ -1,15 +1,73 @@
+from dataclasses import dataclass
+from datetime import datetime, timedelta
 from enum import Enum
-from typing import Optional
+from typing import Optional, List
 
 
-class NaturalDays(Enum):
-    YESTERDAY = ("yesterday", -1)
-    TODAY = ("today", 0)
-    TOMORROW = ("tomorrow", 1)
+@dataclass
+class NaturalDayValue:
+    keywords: List
+    index: int
 
 
-index_vs_natural_days = {n.value[1]: n.value[0] for n in NaturalDays}
+class NaturalDay(Enum):
+    YESTERDAY = NaturalDayValue(keywords=["yesterday"], index=-1)
+    TODAY = NaturalDayValue(keywords=["today"], index=0)
+    TOMORROW = NaturalDayValue(keywords=["tomorrow"], index=1)
+
+
+index_vs_natural_days = {n.value.index: n.value.keywords for n in NaturalDay}
+
+keyword_vs_natural_day = {
+    keyword: n for n in NaturalDay for keyword in n.value.keywords
+}
 
 
 def get_natural_day_or_none(day_index: int) -> Optional[str]:
     return index_vs_natural_days.get(day_index)
+
+
+def convert_from_keyword(keyword: str) -> Optional[datetime]:
+    natural_day = keyword_vs_natural_day.get(keyword)
+    if not natural_day:
+        return None
+
+    if natural_day == NaturalDay.YESTERDAY:
+        return datetime.now() - timedelta(days=1)
+    elif natural_day == NaturalDay.TODAY:
+        return datetime.now()
+    return datetime.now() + timedelta(days=1)
+
+
+class Month(Enum):
+    JANUARY = ("january", "jan")
+    FEB = ("february", "feb")
+    MARCH = ("march", "mar")
+    APRIL = ("april", "apr")
+    MAY = ("may",)
+    JUNE = ("june", "jun")
+    JULY = ("july", "jul")
+    AUGUST = ("august", "aug")
+    SEPTEMBER = ("september", "sep", "sept")
+    OCTOBER = ("october", "oct")
+    NOVEMBER = ("november", "nov")
+    DECEMBER = ("december", "dec")
+
+
+keyword_vs_month = {keyword: m for m in Month for keyword in m.value}
+
+
+def get_month_for_keyword(keyword: str) -> Optional[Month]:
+    return keyword_vs_month.get(keyword)
+
+
+class Period(Enum):
+    AM = ("a.m", "am")
+    PM = ("p.m", "pm")
+
+
+keyword_vs_period = {keyword: p for p in Period for keyword in p.value}
+
+
+def get_period_for_keyword(keyword: str) -> Optional[Month]:
+    return keyword_vs_period.get(keyword)
---
+++ pychronic/models/data_types.py
@@ -0,0 +1,12 @@
+from enum import Enum
+
+
+class DataType(Enum):
+    INT = "int"
+    DATE_OF_MONTH = "date"
+    DATE_MONTH = "date_month"
+    DATE_MONTH_YEAR = "date_month_year"
+    HOUR_MIN_SEC = "hour_min_sec"
+    PERIOD = "period"
+    TIME = "time"
+    TEXT = "text"
